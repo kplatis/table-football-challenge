d6b9bf6ba34f2c6980a1e1b532dd1bcf0866c3f9
kplatis <kplatis@protonmail.com>
2024-02-11 19:27:22 +0100

    Add linting in ci

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,20 +1,19 @@
-
-
 // Import styles of packages that you've installed.
 // All packages except `@mantine/hooks` require styles imports
-import '@mantine/core/styles.css';
+import '@mantine/core/styles.css'
 
-import { ColorSchemeScript, MantineProvider } from '@mantine/core';
+import { ColorSchemeScript, Container, MantineProvider } from '@mantine/core'
+import { Header } from '@/components/Header'
 
 export const metadata = {
   title: 'My Mantine app',
   description: 'I have followed setup instructions carefully',
-};
+}
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode;
+  children: React.ReactNode
 }) {
   return (
     <html lang="en">
@@ -22,8 +21,13 @@ export default function RootLayout({
         <ColorSchemeScript />
       </head>
       <body>
-        <MantineProvider>{children}</MantineProvider>
+        <MantineProvider>
+          <Container size="md">
+            <Header />
+            {children}
+          </Container>
+        </MantineProvider>
       </body>
     </html>
-  );
+  )
 }

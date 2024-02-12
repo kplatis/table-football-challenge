c72341224f8d58cc72c23122c9960847bc767630
kplatis <kplatis@protonmail.com>
2024-02-12 19:52:21 +0100

    Add games listing

--- frontend/src/components/games/GameCard/index.tsx
+++ frontend/src/components/games/GameCard/index.tsx
@@ -1,6 +1,7 @@
 import { Game } from '@/types/games'
 import { Card, Group, Text, Stack, Title, Center } from '@mantine/core'
 import classes from './GamesCard.module.css'
+import TeamBadge from '@/components/teams/TeamBadge'
 
 type Props = {
   game: Game
@@ -8,26 +9,52 @@ type Props = {
 
 export default function GameCard({ game }: Props) {
   return (
-    <Card withBorder padding="lg" className={classes.card}>
-      <Center>
-        <Title order={3}>
-          {game.first_team.name} vs {game.second_team.name}
-        </Title>
-      </Center>
-      <Text mt="sm" mb="md" c="dimmed" fz="xs">
-        HERE ADD THE PLAYERS
-      </Text>
-      <Card.Section className={classes.footer}>
-        <Stack>
-          <Center>
-            <Group>
-              <div>{game.first_team_goals}</div>
-              <div>-</div>
-              <div>{game.second_team_goals}</div>
+    <Card withBorder p="xl" radius="md" className={classes.card}>
+      <Group>
+        <div>
+          <Stack>
+            <Group justify="center" gap="lg" grow>
+              <Stack>
+                <Text size="xl" fw={700} ta={'center'}>
+                  {game.first_team.name}
+                </Text>
+                <Text size="lg" ta={'center'}>
+                  {game.first_team_goals}
+                </Text>
+              </Stack>
+              <Text size="xl" fw={700} ta={'center'}>
+                vs
+              </Text>
+              <Stack>
+                <Text size="xl" fw={700} ta={'center'}>
+                  {game.second_team.name}
+                </Text>
+                <Text size="lg" ta={'center'}>
+                  {game.second_team_goals}
+                </Text>
+              </Stack>
             </Group>
-          </Center>
-        </Stack>
-      </Card.Section>
+            <div>
+              <Text fz="md" mb={6} className={classes.label}>
+                {game.first_team.name}
+              </Text>
+              <TeamBadge
+                firstPlayer={game.first_team.first_player}
+                secondPlayer={game.first_team.second_player}
+              />
+            </div>
+            <div>
+              <Text fz="md" mb={6} className={classes.label}>
+                {game.second_team.name}
+              </Text>
+              <TeamBadge
+                firstPlayer={game.second_team.first_player}
+                secondPlayer={game.second_team.second_player}
+              />
+            </div>
+          </Stack>
+        </div>
+      </Group>
     </Card>
   )
 }

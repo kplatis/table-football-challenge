f64729d59689d2ff0f224643b759af6dc2ecf04e
kplatis <kplatis@protonmail.com>
2024-02-13 00:09:55 +0100

    comment

--- frontend/src/components/games/PlayedGameCreationForm/validation.ts
+++ frontend/src/components/games/PlayedGameCreationForm/validation.ts
@@ -1,17 +1,17 @@
 import { GameCreationDataPreValidation } from './types'
 
 /**
- * Validation function for first player
+ * Validation function for first team
  */
 export function validateFirstTeam(
   value: number,
   values: GameCreationDataPreValidation,
 ) {
-  // if first player value is not set
+  // if first team value is not set
   if (value === null) {
     return 'First Team is required'
   }
-  // if selected same first and second player
+  // if selected same first and second team
   if (values.secondTeamId && value === values.secondTeamId) {
     return 'First and second team cannot be the same'
   }
@@ -19,17 +19,17 @@ export function validateFirstTeam(
 }
 
 /**
- * Validation function for first player
+ * Validation function for first team
  */
 export function validateSecondTeam(
   value: number,
   values: GameCreationDataPreValidation,
 ) {
-  // if first player value is not set
+  // if first team value is not set
   if (value === null) {
     return 'Second Team is required'
   }
-  // if selected same first and second player
+  // if selected same first and second team
   if (values.firstTeamId && value === values.firstTeamId) {
     return 'First and second team cannot be the same'
   }

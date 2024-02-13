9b9ca5787e41246c9d237761bf2bff9fde1a960b
Konstantinos Platis <5221058+kplatis@users.noreply.github.com>
2024-02-13 21:47:21 +0100

    Add query parameter in /statistics/overview to filter players/teams

--- backend/api/statistics/routers.py
+++ backend/api/statistics/routers.py
@@ -4,7 +4,7 @@ Router declaration for Statistics module
 
 from typing import List
 from sqlalchemy.orm import Session
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, Query
 from api.dependencies import get_db
 from api.statistics.crud import (
     get_statistics_dictionary_of_all_teams,
@@ -19,7 +19,12 @@ router = APIRouter()
 @router.get(
     "/overview", tags=["Statistics"], response_model=List[StatisticsByTeamOrPlayer]
 )
-async def get_statistics_overview_for_teams_and_players(db: Session = Depends(get_db)):
+async def get_statistics_overview_for_teams_and_players(
+    category: str = Query(
+        None, description="Category of the item", pattern="^(players|teams)$"
+    ),
+    db: Session = Depends(get_db),
+):
     """
     Endpoint to retrieve statistics overview for teams and players
     """
@@ -29,5 +34,10 @@ async def get_statistics_overview_for_teams_and_players(db: Session = Depends(ge
     player_statistics = get_statistics_of_all_players(
         team_ids_to_stats=team_ids_to_statistics, db=db
     )
-    team_response = list(team_ids_to_statistics.values()) + player_statistics
-    return [StatisticsByTeamOrPlayer(**response) for response in team_response]
+    if category == "players":
+        response = player_statistics
+    elif category == "teams":
+        response = list(team_ids_to_statistics.values())
+    else:
+        response = list(team_ids_to_statistics.values()) + player_statistics
+    return [StatisticsByTeamOrPlayer(**response) for response in response]

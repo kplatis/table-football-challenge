6e08c30cf09913bf08aeac3dbd627099cc884464
kplatis <kplatis@protonmail.com>
2024-02-14 22:19:58 +0100

    Move function and re-use

---
+++ frontend/src/components/teams/TeamComparison/index.tsx
@@ -0,0 +1,49 @@
+'use client'
+
+import { Team } from '@/types/teams'
+import TeamSelectionForm from '../TeamSelectionForm'
+import { useState } from 'react'
+import useTeams from '@/hooks/useTeams'
+import { Center, Loader } from '@mantine/core'
+
+type TeamSelectioState = {
+  firstTeam?: Team
+  secondTeam?: Team
+}
+
+export default function TeamComparison() {
+  const { isLoading, data } = useTeams()
+  const [selectedTeams, setSelectedTeams] = useState<TeamSelectioState>({
+    firstTeam: undefined,
+    secondTeam: undefined,
+  })
+
+  const setSelected = (firstTeamId: number, secondTeamId: number) => {
+    const firstTeam = data?.find((team) => team.id === firstTeamId)
+    const secondTeam = data?.find((team) => team.id === secondTeamId)
+
+    setSelectedTeams({
+      firstTeam: firstTeam,
+      secondTeam: secondTeam,
+    })
+  }
+
+  if (isLoading) {
+    return (
+      <Center>
+        <Loader color="blue" />
+      </Center>
+    )
+  }
+  if (data) {
+    return (
+      <>
+        <TeamSelectionForm
+          teams={data}
+          submissionButtonText="Compare!"
+          setTeamsFn={setSelected}
+        />
+      </>
+    )
+  }
+}

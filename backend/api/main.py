7c1c455dc49845574f8eed959cef1e558d937501
kplatis <kplatis@protonmail.com>
2024-02-14 16:24:25 +0100

    add function for team

--- backend/api/main.py
+++ backend/api/main.py
@@ -8,7 +8,8 @@ from api.database import Base, engine
 from api.teams import routers as team_routers
 from api.players import routers as players_routers
 from api.games import routers as games_routers
-from api.statistics import routers as stats_routers
+
+# from api.statistics import routers as stats_routers
 
 
 Base.metadata.create_all(bind=engine)
@@ -45,4 +46,4 @@ app.add_middleware(
 app.include_router(players_routers.router, prefix="/players")
 app.include_router(team_routers.router, prefix="/teams")
 app.include_router(games_routers.router, prefix="/games")
-app.include_router(stats_routers.router, prefix="/statistics")
+# app.include_router(stats_routers.router, prefix="/statistics")
--- backend/api/statistics/calculator.py
+++ backend/api/statistics/calculator.py
@@ -37,6 +37,26 @@ def add_stats(stats, team_won: bool, goals_scored: int, goals_perceived: int):
     }
 
 
+def calculate_stats_for_team(
+    name: str,
+    teams_stats,
+    team_id: int,
+    team_won: bool,
+    goals_scored: int,
+    goals_perceived: int,
+):
+
+    # if team not in the dict, initialize
+    if team_id not in teams_stats:
+        teams_stats[team_id] = initialize_stats(name)
+    # add the home team stats for this game
+    teams_stats[team_id] = add_stats(
+        teams_stats[team_id], team_won, goals_scored, goals_perceived
+    )
+
+    return teams_stats
+
+
 def calculate_stats_for_teams_and_players(games: List[Game]):
 
     teams = {}
@@ -50,19 +70,25 @@ def calculate_stats_for_teams_and_players(games: List[Game]):
         away_team_goals = game.second_team_goals
         home_team_won = home_team_goals > away_team_goals
         away_team_won = home_team_goals < away_team_goals
-        # if team not in the dict, initialize
-        if game.first_team_id not in teams:
-            teams[game.first_team_id] = initialize_stats(game.first_team.name)
-        # add the home team stats for this game
-        teams[game.first_team_id] = add_stats(
-            teams[game.first_team_id], home_team_won, home_team_goals, away_team_goals
+
+        # calculating stats for home team
+        teams = calculate_stats_for_team(
+            game.first_team.name,
+            teams,
+            game.first_team_id,
+            home_team_won,
+            home_team_goals,
+            away_team_goals,
         )
-        # if team not in the dict, initialize
-        if game.second_team_id not in teams:
-            teams[game.second_team_id] = initialize_stats(game.second_team.name)
-        # add the away team stats for this game
-        teams[game.second_team_id] = add_stats(
-            teams[game.second_team_id], away_team_won, away_team_goals, home_team_goals
+
+        # calculating stats for away team
+        teams = calculate_stats_for_team(
+            game.second_team.name,
+            teams,
+            game.second_team_id,
+            away_team_won,
+            away_team_goals,
+            home_team_goals,
         )
 
         # PLAYERS CALCULATION
@@ -119,80 +145,79 @@ def calculate_stats_for_teams_and_players(games: List[Game]):
                 home_team_goals,
             )
 
-        # TODO: do the same for players
     print(1)
 
 
-def calculate_stats_for_team(games: Game, team_id: int):
-    """
-    Calculates the statistics of all games
-    """
-    stats = {
-        "wins": 0,
-        "losses": 0,
-        "win_ratio": 0.0,
-        "goals_for": 0,
-        "goals_against": 0,
-        "goals_difference": 0,
-    }
-    # iterate over each game and update stats
-    for game in games:
-        # depending on which team is the one checking, setting the values of goals scored and perceived
-        goals_scored_in_game = getattr(
-            game,
-            (
-                "first_team_goals"
-                if game.first_team_id == team_id
-                else "second_team_goals"
-            ),
-        )
-        goals_perceived_in_game = getattr(
-            game,
-            (
-                "second_team_goals"
-                if game.first_team_id == team_id
-                else "first_team_goals"
-            ),
-        )
+# def calculate_stats_for_team(games: Game, team_id: int):
+#     """
+#     Calculates the statistics of all games
+#     """
+#     stats = {
+#         "wins": 0,
+#         "losses": 0,
+#         "win_ratio": 0.0,
+#         "goals_for": 0,
+#         "goals_against": 0,
+#         "goals_difference": 0,
+#     }
+#     # iterate over each game and update stats
+#     for game in games:
+#         # depending on which team is the one checking, setting the values of goals scored and perceived
+#         goals_scored_in_game = getattr(
+#             game,
+#             (
+#                 "first_team_goals"
+#                 if game.first_team_id == team_id
+#                 else "second_team_goals"
+#             ),
+#         )
+#         goals_perceived_in_game = getattr(
+#             game,
+#             (
+#                 "second_team_goals"
+#                 if game.first_team_id == team_id
+#                 else "first_team_goals"
+#             ),
+#         )
 
-        # given there is no draw
-        if goals_scored_in_game > goals_perceived_in_game:
-            stats["wins"] += 1
-        else:
-            stats["losses"] += 1
-        stats["goals_for"] += goals_scored_in_game
-        stats["goals_against"] += goals_perceived_in_game
-    stats["goals_difference"] = stats["goals_for"] - stats["goals_against"]
-    total_matches = stats["wins"] + stats["losses"]
-    win_percentage = stats["wins"] / total_matches if total_matches > 0 else 0.0
+#         # given there is no draw
+#         if goals_scored_in_game > goals_perceived_in_game:
+#             stats["wins"] += 1
+#         else:
+#             stats["losses"] += 1
+#         stats["goals_for"] += goals_scored_in_game
+#         stats["goals_against"] += goals_perceived_in_game
+#     stats["goals_difference"] = stats["goals_for"] - stats["goals_against"]
+#     total_matches = stats["wins"] + stats["losses"]
+#     win_percentage = stats["wins"] / total_matches if total_matches > 0 else 0.0
 
-    stats["win_ratio"] = float(f"{win_percentage:.2f}")
-    return stats
+#     stats["win_ratio"] = float(f"{win_percentage:.2f}")
+#     return stats
 
 
-def calculate_stats_for_player(
-    teams_of_player: List[int], team_ids_to_stats: Dict[int, Dict]
-):
-    """
-    Calculates the stats for a player, given a list of the IDs of the teams that the player
-    took part in (teams_of_players) and a dictionary with the Ids of the teams and the stats
-    """
-    stats = {
-        "wins": 0,
-        "losses": 0,
-        "win_ratio": 0.0,
-        "goals_for": 0,
-        "goals_against": 0,
-        "goals_difference": 0,
-    }
-    for team_id in teams_of_player:
-        team_stats = team_ids_to_stats[team_id]
-        stats["wins"] += team_stats["wins"]
-        stats["losses"] += team_stats["losses"]
-        stats["goals_for"] += team_stats["goals_for"]
-        stats["goals_against"] += team_stats["goals_against"]
-        stats["goals_difference"] += team_stats["goals_difference"]
-    total_matches = stats["wins"] + stats["losses"]
-    win_percentage = stats["wins"] / total_matches if total_matches > 0 else 0.0
-    stats["win_ratio"] = float(f"{win_percentage:.2f}")
-    return stats
+# def calculate_stats_for_player(
+#     teams_of_player: List[int], team_ids_to_stats: Dict[int, Dict]
+# ):
+#     """
+#     Calculates the stats for a player, given a list of the IDs of the teams that the player
+#     took part in (teams_of_players) and a dictionary with the Ids of the teams and the stats
+#     """
+#     stats = {
+#         "wins": 0,
+#         "losses": 0,
+#         "win_ratio": 0.0,
+#         "goals_for": 0,
+#         "goals_against": 0,
+#         "goals_difference": 0,
+#     }
+#     for team_id in teams_of_player:
+#         team_stats = team_ids_to_stats[team_id]
+#         stats["wins"] += team_stats["wins"]
+#         stats["losses"] += team_stats["losses"]
+#         stats["goals_for"] += team_stats["goals_for"]
+#         stats["goals_against"] += team_stats["goals_against"]
+#         stats["goals_difference"] += team_stats["goals_difference"]
+#     total_matches = stats["wins"] + stats["losses"]
+#     win_percentage = stats["wins"] / total_matches if total_matches > 0 else 0.0
+#     stats["win_ratio"] = float(f"{win_percentage:.2f}")
+#     return stats

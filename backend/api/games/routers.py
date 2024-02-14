a83a7b6ec6dddf6e98e5d32f30bc63daa941a068
kplatis <kplatis@protonmail.com>
2024-02-14 18:06:14 +0100

    Fix response

--- backend/api/games/routers.py
+++ backend/api/games/routers.py
@@ -12,13 +12,18 @@ from api.exceptions import (
     SameTeamsGameException,
     TeamDoesNotExistException,
 )
-from api.games import schemas, crud
+from api.games import schemas as games_schemas, crud
+from api.teams import schemas as teams_schemas
+from api.players import schemas as players_schemas
+
 
 router = APIRouter()
 
 
-@router.post("", tags=["Games"], response_model=schemas.Game)
-async def create_new_game(game: schemas.GameCreate, db: Session = Depends(get_db)):
+@router.post("", tags=["Games"], response_model=games_schemas.Game)
+async def create_new_game(
+    game: games_schemas.GameCreate, db: Session = Depends(get_db)
+):
     """
     Defines endpoint to create a new game
     """
@@ -34,7 +39,7 @@ async def create_new_game(game: schemas.GameCreate, db: Session = Depends(get_db
         ) from exc
 
 
-@router.get("", tags=["Games"], response_model=schemas.GameListResponse)
+@router.get("", tags=["Games"], response_model=games_schemas.GameListResponse)
 async def list_all_games(
     with_team_stats: bool = False,
     with_player_stats: bool = False,
@@ -46,16 +51,53 @@ async def list_all_games(
     games = crud.list_games(db)
     if with_team_stats or with_player_stats:
         teams, players = calculate_stats_for_teams_and_players(games=games)
-    return schemas.GameListResponse(
-        games=games,
-        team_stats=list(teams.items()) if with_team_stats else None,
-        player_stats=list(players.items()) if with_player_stats else None,
+    return games_schemas.GameListResponse(
+        games=[
+            games_schemas.Game.model_validate(
+                {
+                    **game.__dict__,
+                    "first_team": teams_schemas.Team.model_validate(
+                        {
+                            **game.first_team.__dict__,
+                            "first_player": players_schemas.Player.model_validate(
+                                game.first_team.first_player.__dict__
+                            ),
+                            "second_player": (
+                                players_schemas.Player.model_validate(
+                                    game.first_team.second_player.__dict__
+                                )
+                                if game.first_team.second_player
+                                else None
+                            ),
+                        }
+                    ),
+                    "second_team": teams_schemas.Team.model_validate(
+                        {
+                            **game.second_team.__dict__,
+                            "first_player": players_schemas.Player.model_validate(
+                                game.second_team.first_player.__dict__
+                            ),
+                            "second_player": (
+                                players_schemas.Player.model_validate(
+                                    game.second_team.second_player.__dict__
+                                )
+                                if game.second_team.second_player
+                                else None
+                            ),
+                        }
+                    ),
+                }
+            )
+            for game in games
+        ],
+        team_stats=list(teams.values()) if with_team_stats else None,
+        player_stats=list(players.values()) if with_player_stats else None,
     )
 
 
-@router.patch("/{game_id}", tags=["Games"], response_model=schemas.Game)
+@router.patch("/{game_id}", tags=["Games"], response_model=games_schemas.Game)
 async def patch_game(
-    game_id: int, game: schemas.GamePartialUpdate, db: Session = Depends(get_db)
+    game_id: int, game: games_schemas.GamePartialUpdate, db: Session = Depends(get_db)
 ):
     """
     Defines endpoint to list all games
@@ -66,7 +108,7 @@ async def patch_game(
         raise HTTPException(status_code=404, detail="Game does not exist") from exc
 
 
-@router.get("/{game_id}", tags=["Games"], response_model=schemas.Game)
+@router.get("/{game_id}", tags=["Games"], response_model=games_schemas.Game)
 async def get_game(game_id: int, db: Session = Depends(get_db)):
     """
     Defines endpoint to retrieve a single game

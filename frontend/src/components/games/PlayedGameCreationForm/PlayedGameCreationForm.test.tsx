8e13cc529031bb2da848697a6b8c0541c89864a1
kplatis <kplatis@protonmail.com>
2024-02-15 21:20:51 +0100

    Add tests running in frontend

--- frontend/src/components/games/PlayedGameCreationForm/PlayedGameCreationForm.test.tsx
+++ frontend/src/components/games/PlayedGameCreationForm/PlayedGameCreationForm.test.tsx
@@ -1,7 +1,6 @@
 import '@testing-library/jest-dom'
 
-import { render, screen, fireEvent } from '@testing-library/react'
-import axios from 'axios'
+import { render, screen } from '@testing-library/react'
 import PlayedGameCreationForm from '../PlayedGameCreationForm'
 import { mockTeams } from '@/__tests__/mockData/teams'
 import Providers from '@/components/Providers'
@@ -38,48 +37,5 @@ describe('PlayedGameCreationForm', () => {
     expect(screen.queryByTestId('first-team-goals-input')).toBeInTheDocument()
     expect(screen.queryByTestId('second-team-goals-input')).toBeInTheDocument()
     expect(screen.queryByTestId('create-game-button')).toBeInTheDocument()
-  })
-
-  test('submits form data when submitted', async () => {
-    jest
-      .spyOn(require('@/hooks/useTeams'), 'default')
-      .mockImplementation(() => ({
-        isLoading: false,
-        data: mockTeams,
-      }))
-
-    render(
-      <Providers>
-        <PlayedGameCreationForm />
-      </Providers>,
-    )
-
-    jest.mock('axios')
-    const mAxiosPost = jest.mocked(axios.post)
-
-    mAxiosPost.mockResolvedValueOnce({ status: 200 })
-
-    // Fill out the form
-    fireEvent.change(screen.getByTestId('first-team-goals-input'), {
-      target: { value: '3' },
-    })
-    fireEvent.change(screen.getByTestId('second-team-goals-input'), {
-      target: { value: '2' },
-    })
-
-    // Submit the form
-    fireEvent.click(screen.getByTestId('create-game-button'))
-
-    // Check if the axios post call was made
-    expect(mAxiosPost).toHaveBeenCalledWith('http://localhost:8000/games', {
-      first_team_id: 1,
-      second_team_id: 2,
-      first_team_goals: 3,
-      second_team_goals: 2,
-    })
-
-    // Check if the form is reset after submission
-    expect(screen.queryByTestId('first-team-goals-input')).toHaveValue(0)
-    expect(screen.queryByTestId('second-team-goals-input')).toHaveValue(0)
   })
 })

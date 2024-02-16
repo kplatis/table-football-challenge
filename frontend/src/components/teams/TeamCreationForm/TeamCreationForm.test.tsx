8ffd2035ee135b847c7e3dd70486dd372f2faef1
kplatis <kplatis@protonmail.com>
2024-02-16 09:01:00 +0100

    add teamsgrid tests

--- frontend/src/components/teams/TeamCreationForm/TeamCreationForm.test.tsx
+++ frontend/src/components/teams/TeamCreationForm/TeamCreationForm.test.tsx
@@ -1,7 +1,5 @@
 import '@testing-library/jest-dom'
-import { render, screen, waitFor } from '@testing-library/react'
-import userEvent from '@testing-library/user-event'
-import axios from 'axios'
+import { render, screen } from '@testing-library/react'
 import TeamCreationForm from '../TeamCreationForm'
 import { mockPlayers } from '@/__tests__/mockData/players'
 import Providers from '@/components/Providers'
